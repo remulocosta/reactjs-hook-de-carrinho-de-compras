6babb0276b5f59bc07b23b9289ab01c14be3fd11
Remulo Costa <remulo.costa@vulcanet.com.br>
2022-05-15 18:50:43 -0300

    Update page Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
+import {AxiosResponse} from 'axios';
 
 import { ProductList } from './styles';
 import { api } from '../../services/api';
@@ -22,44 +23,79 @@ interface CartItemsAmount {
 }
 
 const Home = (): JSX.Element => {
-  // const [products, setProducts] = useState<ProductFormatted[]>([]);
-  // const { addProduct, cart } = useCart();
+  const [products, setProducts] = useState<ProductFormatted[]>([]);
+  const { addProduct, cart } = useCart();
 
-  // const cartItemsAmount = cart.reduce((sumAmount, product) => {
-  //   // TODO
-  // }, {} as CartItemsAmount)
+  const cartItemsAmount = cart.reduce((sumAmount, product) => {
+    sumAmount[product.id] = product.amount
+
+    return sumAmount
+  }, {} as CartItemsAmount)
 
   useEffect(() => {
     async function loadProducts() {
-      // TODO
+      let result: AxiosResponse<any> | null;
+
+      try {
+        result = await api.get('/products');
+      }
+      catch(e) {
+        console.log('Ocorreu um erro ao recuperar os produtos.', e);
+        return
+      }
+
+      if (result === null) {
+          console.log('Sem produtos.');
+          return
+      }
+
+      const formattedItens = result.data.map((product: Product) => {
+            return {
+                ...product,
+                priceFormatted: formatPrice(product.price)
+            }
+        });
+
+      setProducts(formattedItens)
     }
 
     loadProducts();
   }, []);
 
   function handleAddProduct(id: number) {
-    // TODO
+    addProduct(id);
+  }
+
+  function renderProducts(product: ProductFormatted): JSX.Element {
+    return (
+        <li key={product.id}>
+            <img
+                src={product.image}
+                alt={product.title}
+            />
+            <strong>{product.title}</strong>
+            <span>{product.priceFormatted}</span>
+            <button
+                type="button"
+                data-testid="add-product-button"
+                onClick={() => handleAddProduct(product.id)}
+            >
+            <div data-testid="cart-product-quantity">
+                <MdAddShoppingCart size={16} color="#FFF" />
+                {cartItemsAmount[product.id] || 0}
+            </div>
+
+            <span>ADICIONAR AO CARRINHO</span>
+            </button>
+        </li>
+    )
   }
 
   return (
     <ProductList>
-      <li>
-        <img src="https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg" alt="Tênis de Caminhada Leve Confortável" />
-        <strong>Tênis de Caminhada Leve Confortável</strong>
-        <span>R$ 179,90</span>
-        <button
-          type="button"
-          data-testid="add-product-button"
-        // onClick={() => handleAddProduct(product.id)}
-        >
-          <div data-testid="cart-product-quantity">
-            <MdAddShoppingCart size={16} color="#FFF" />
-            {/* {cartItemsAmount[product.id] || 0} */} 2
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
+        {products.length > 0 &&
+            products.map(renderProducts)
+        }
     </ProductList>
   );
 };
